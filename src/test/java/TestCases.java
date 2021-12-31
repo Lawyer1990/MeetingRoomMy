4919b8c7f39b9f1935f5a494f9bbb2d89db64e7e
Aliaksei Skliutau <a.skliutau@andersenlab.com>
2021-12-31 11:31:35 +0300

    first commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+    @Test
+    public void test1() {
+        System.out.println("test1 - done");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test2 - done");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("test3 - done");
+    }
+}
